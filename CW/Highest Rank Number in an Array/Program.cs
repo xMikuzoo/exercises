abbda47e7bb415fd50792a2ef45c18fcad573819
Wojciech Król <wojciech.krol@adminbrothers.com>
2024-11-21 15:51:17 +0100

    init Highest Rank Number in an Array

---
+++ CW/Highest Rank Number in an Array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highest_Rank_Number_in_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CW/Highest Rank Number in an Array/README.md
@@ -0,0 +1,3 @@
+## 🧩 Odnajdź zadanie na CodeWars w łatwy sposób 🖥️
+
+[🔗 Link do zadania](https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004)
