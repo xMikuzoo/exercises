5cbf45347d5c2c178376e06712c956dfa9b7bc89
Wojciech Król <wojciech.krol@adminbrothers.com>
2024-11-21 16:04:59 +0100

    Highest_Rank_Number_in_an_Array to end

--- CW/Highest Rank Number in an Array/Program.cs
+++ CW/Highest Rank Number in an Array/Program.cs
@@ -10,6 +10,19 @@ namespace Highest_Rank_Number_in_an_Array
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(HighestRank(12, 10, 8, 12, 7, 6, 4, 10, 12));
+            Console.WriteLine(HighestRank(12, 10, 8, 8, 3, 3, 3, 3, 2, 2, 2, 2, 4, 10, 12, 10));
+        }
+
+        public static int HighestRank(params int[] arr)
+        {
+            var highestRank = arr
+                .GroupBy(x => x)
+                .Select(x => new { Number = x.Key, Count = x.Count() })
+                .OrderByDescending(x => x.Count)
+                .OrderByDescending (x => x.Number)
+                .FirstOrDefault();
+            return highestRank.Number;
         }
     }
 }
